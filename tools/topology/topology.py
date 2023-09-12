6c2efa7f0f7889b3be4d80d46655960134cecc43
Lukas Heine <lu.heine@tum.de>
2023-09-12 17:28:05 +0200

    ff TreeMatch optimizer

--- tools/topology/topology.py
+++ tools/topology/topology.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import argparse
+from functools import reduce
 import igraph
 import itertools
 import math
@@ -134,6 +135,13 @@ def generateHostGraph(hostnames: list[str]) -> HostGraph:
     return HostGraph(topGraph, layers)
 
 
+def getNodeChildren(graph: HostGraph, node: str, nodelayer: int) -> list:
+    if nodelayer + 1 >= len(graph.layers):
+        return []
+    connected_nodes = set(graph.graph.vs[graph.graph.neighbors(node)]["name"]).intersection(graph.layers[nodelayer + 1])
+    return list(connected_nodes)
+
+
 # solve the embedding problem for given graphs and return acceptable reordering
 # todo we probably need the hostnames sooner or later
 #   for now on test we always get successive hosts
@@ -149,6 +157,7 @@ def treeMatch(comm_mat, top_graph, hostnames) -> list:
 class TreeMatch:
     def __init__(self, comm_mat: list, top_graph: HostGraph, hostnames: list[str]):
         self.comm_mat = comm_mat
+        self.mod_mat = comm_mat
         self.top_graph = top_graph
         self.hostnames = hostnames
 
@@ -163,21 +172,28 @@ class TreeMatch:
 
     def doTreeMatch(self):
         groups = [[] for _ in range(len(self.top_graph.layers))]
-        tmp_mat = self.comm_mat
+
+        print("Starting TreeMatch for {} groups.".format(len(groups)))
+
         for d in range(len(self.top_graph.layers) - 1, 1, -1):
-            p = len(tmp_mat)
+            p = len(self.mod_mat)
             if p % self.arity(d - 1) != 0:
-                tmp_mat = self.extendCommMatrix(d)
-            groups[d] = self.groupProcesses(d)
-            tmp_mat = self.aggregateCommMatrix(groups[d])
+                self.mod_mat = self.extendCommMatrix(d)
+            groups[d] = self.groupProcesses(self.mod_mat, d)
+            self.mod_mat = self.aggregateCommMatrix(groups[d])
         return [x for xs in groups[-1] for x in xs]
 
-    def groupProcesses(self, cur_depth):
-        UINT64_MAX = 0xffffffffffffffff
+    def groupProcesses(self, matrix: list, cur_depth: int):
+        # TODO: this only works for balanced trees for now
+        UINT64_MAX = 0xFFFFFFFFFFFFFFFF
         # print("processes         ", len(self.hostnames))
         # print("groupsize         ", self.arity(cur_depth - 1))
+        # TODO: this should only work with connected nodes instead of the entire layer
         l = list(itertools.combinations(self.top_graph.layers[cur_depth], self.arity(cur_depth - 1)))
-        # print("combinations[0]   ", l[0])
+
+        print("Grouping process combinations with arity {} at depth {}.".format(self.arity(cur_depth - 1), cur_depth))
+
+        # print("combinations      ", l)
         # print("num_combinations  ", len(l))
         d = dict(list(map(lambda x: (hash(x) & UINT64_MAX, x), l)))
         G = igraph.Graph()
@@ -189,14 +205,63 @@ class TreeMatch:
                 for node in group1:
                     if node in group2:
                         G.add_edge(str(hash(group1) & UINT64_MAX), str(hash(group2) & UINT64_MAX))
+                        # print("incompatible groups {} ~ {} added to set.".format(str(group1), str(group2)))
+
         # print("graph_groups      ", len(G.vs))
         # print("groups to find    ", len(self.comm_mat) // self.arity(cur_depth - 1))
         # TODO: improve independent set algorithm
-        independent_set = G.independent_vertex_sets(len(self.comm_mat) // self.arity(cur_depth - 1), len(self.comm_mat) // self.arity(cur_depth - 1))
+        independent_set = G.independent_vertex_sets(
+            len(self.comm_mat) // self.arity(cur_depth - 1), len(self.comm_mat) // self.arity(cur_depth - 1)
+        )
+
+        print("found {} possible combinations out of {} groups.".format(len(independent_set), len(G.vs)))
+
+        print(
+            "node children of {}: {}".format(
+                self.top_graph.layers[cur_depth][0],
+                getNodeChildren(self.top_graph, self.top_graph.layers[cur_depth][0], cur_depth),
+            )
+        )
+
         # print("independentset[0] ", independent_set[0], l[independent_set[0][0]], l[independent_set[0][1]])
         # TODO: filter the sets greedy
-        return [l[g] for g in independent_set[0]]
+        min_group = []
+        min_weight = 0xFFFFFFFF
+        for matching in independent_set:
+            cur_weight = self.groupWeight(matrix, [l[g] for g in matching])
+            if cur_weight < min_weight:
+                min_group = [l[g] for g in matching]
+                min_weight = cur_weight
 
+        return min_group
+
+    # return the number of cross-node groups
+    # TODO factor in the actual transfer cost in this
+    # weight should just be the total communication cost of this group
+    # TODO this only works on leaf level like this
+    def groupWeight(self, matrix, group):
+        weight = 0
+        if len(group) == 0:
+            return weight
+        # for match in group:
+            # TODO: calculate ranking based on communication matrix
+            # use the outbound group communication to weigh
+            # matchset = set(list(map(lambda x: x[:12], match)))
+            # weight += len(matchset) - 1
+
+        # print("calculating outbound communication for group {}".format(group))
+
+        for match in group:
+            weight -= self.elementSum([match], 0, 0)
+
+        # exit()
+
+
+        print("calculated weight {} for group {}".format(weight, group))
+
+        return weight
+
+    # aggregate submatrices according to groups
     def aggregateCommMatrix(self, groups):
         n = len(groups)
         r = [[0 for _ in range(n)] for _ in range(n)]
@@ -206,11 +271,15 @@ class TreeMatch:
                     r[i][j] = self.elementSum(groups, i, j)
         return r
 
+    # element wise sum across group submatrices
     def elementSum(self, groups, i, j):
         acc = 0
         for a in groups[i]:
             for b in groups[j]:
                 acc += self.comm_mat[self.hostnames.index(a)][self.hostnames.index(b)]
+
+        # print("element wise sum for group {}: {}".format(groups, acc))
+
         return acc
 
     # extend matrix by arity of lower depth lines and columns
@@ -218,14 +287,13 @@ class TreeMatch:
     # else vertex degree-1 due to minimal graph
     # treematch as described wants balanced nodes, does this change anything? ..
     def extendCommMatrix(self, cur_depth) -> list:
-        p = len(self.comm_mat)
+        p = len(self.mod_mat)
         k = self.arity(cur_depth)
-        ext_mat = self.comm_mat
-        for l in ext_mat:
+        for l in self.mod_mat:
             l.extend([0] * k)
         for _ in range(k):
-            ext_mat.append([0 for _ in range(p)])
-        return ext_mat
+            self.mod_mat.append([0 for _ in range(p)])
+        return self.mod_mat
 
 
 def optimize(optimizer, *args) -> list:
@@ -258,7 +326,7 @@ if __name__ == "__main__":
             )
 
             # for line in cg.commMatrix:
-                # print(line)
+            # print(line)
 
             hostgraph = generateHostGraph(list(map(lambda s: s.strip("'"), cg.hostnames)))
             hostgraphlayout = hostgraph.graph.layout_reingold_tilford(mode="in", root=hostgraph.layers[0])
@@ -276,6 +344,9 @@ if __name__ == "__main__":
             )
 
             print(optimize("treeMatch", cg.commMatrix, hostgraph, list(map(lambda s: s.strip("'"), cg.hostnames))))
+
+            for line in cg.commMatrix:
+                print(line)
 
     # we have an input matrix and topology, optimize and output reordering
     elif args.cg is not None and args.tg is not None:
